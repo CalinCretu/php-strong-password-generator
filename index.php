c252669af5feb90883fd239ebff8e58365579206
CalinCretu <calincretu1412@gmail.com>
2023-11-25 14:18:58 +0100

    FIX - relocate_code

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+    require_once("functions.php");
+?>
+<?php
+
+    if ($_GET["lunghezza"]) {
+        $lunghezza = $_GET["lunghezza"] ?? 0;
+        $password = genera_password($lunghezza);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,20 +22,10 @@
 
   <h3>Generatore di Password quasi affidabile</h3>
   <p>Inserisci il numero di caratteri che comporranno la password e clicca sul pulstante genera</p>
-  <?php
-    require_once("functions.php");
-?>
   <form action="index.php" method="get">
     <input type="number" name="lunghezza" placeholder="Lunghezza password">
     <input type="submit" value="Genera">
   </form>
-  <?php
-
-    if ($_GET["lunghezza"]) {
-        $lunghezza = $_GET["lunghezza"];
-        $password = genera_password($lunghezza);
-    }
-?>
   <h2>La password generata e': <?php echo $password ?></h2>
 </body>
 
